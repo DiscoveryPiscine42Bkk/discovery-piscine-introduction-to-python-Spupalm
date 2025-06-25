0be1f0d92c6854b5c425de2838ca29d798236432
Arecaceae <90172069+Spupalm@users.noreply.github.com>
2025-06-26 02:21:18 +0700

    Add files via upload

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,9 @@
+num = input("Give me a number: ")
+try:
+    number = float(num)
+    if number == int(number):
+        print(int(number))
+    else:
+        print(int(number) + 1)
+except ValueError:
+    print("Invalid Input!")
