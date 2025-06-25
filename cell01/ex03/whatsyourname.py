3479a26eddfb13297de8670bd06d28baf7a7116f
Arecaceae <90172069+Spupalm@users.noreply.github.com>
2025-06-25 14:13:33 +0700

    Add files via upload

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,4 @@
+firstname = input("Hey, what's your first name? :")
+lastname = input("And your last name? :")
+whole_name = f"{firstname} {lastname}"
+print(f"Well, pleased to meet you, {whole_name}.")
