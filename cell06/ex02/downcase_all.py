37cbe98e20116e8d7ccf667bdb0410e9fb847318
Arecaceae <90172069+Spupalm@users.noreply.github.com>
2025-06-26 19:15:22 +0700

    Add files via upload

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,8 @@
+import sys
+def downcase_it(string):
+    return string.lower()
+if len(sys.argv) > 1:
+    print(downcase_it(sys.argv[1]))
+    print("I understood arrays well!")
+else:
+    print("None") 
